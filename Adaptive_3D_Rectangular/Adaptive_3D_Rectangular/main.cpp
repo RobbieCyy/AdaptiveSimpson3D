97e20d53aa591f6946653d49c306959031d6c782
RobbieCyy <1600011328@pku.edu.cn>
2019-03-01 10:16:17 +0800

    A few changes made by VS

--- Adaptive_3D_Rectangular/Adaptive_3D_Rectangular/main.cpp
+++ Adaptive_3D_Rectangular/Adaptive_3D_Rectangular/main.cpp
@@ -1,11 +1,11 @@
-#define FACTOR 15
+#define FACTOR 10
 #define doubel double
 #include <iostream>
 #include <ctime>
 
 using namespace std;
 
-int Func_count = 0;
+long long Func_count = 0;
 
 double My_func3(double *r) {
 	Func_count++;
@@ -30,40 +30,51 @@ double * get_Division(double *a, double *b, int *n) {
 }
 
 double Adaptive_Rectangular_3D(double *a, double *b, double epsilon, double(*f)(double *), int level) {
+	//cout << "level=" << level << " a=[" << a[0] << ' ' << a[1] << ' ' << a[2] << ' ' << "], b=[" << b[0] << ' ' << b[1] << ' ' << b[2] << "]" << endl;
 	double result = Rectangular_3D(a, b, f);
 	double sub_result = 0;
 	if (level > 20) {
 		return result;
 	}
-	for (int x = 0; x < 1; ++x) {
-		for (int y = 0; y < 1; ++y) {
-			for (int z = 0; z < 1; ++z) {
+	for (int x = 0; x < 2; ++x) {
+		for (int y = 0; y < 2; ++y) {
+			for (int z = 0; z < 2; ++z) {
 				int n1[3] = { 0 };
 				int n2[3] = { 0 };
 				n1[0] = x, n1[1] = y, n1[2] = z;
 				n2[0] = x + 1, n2[1] = y + 1, n2[2] = z + 1;
-				sub_result += Rectangular_3D(get_Division(a, b, n1), get_Division(a, b, n2), f);
+				double * aa, *bb;
+				aa = get_Division(a, b, n1);
+				bb = get_Division(a, b, n2);
+				sub_result += Rectangular_3D(aa, bb, f);
+				delete aa;
+				delete bb;
 			}
 		}
 	}
 	if (abs(result - sub_result) < FACTOR * epsilon) {
-		cout << "return point 1" << endl;
+		//cout << "return point 1" << endl;
 		return sub_result + (sub_result - result) / FACTOR;
 	}
 	else {
 		result = 0;
-		for (int x = 0; x < 1; ++x) {
-			for (int y = 0; y < 1; ++y) {
-				for (int z = 0; z < 1; ++z) {
+		for (int x = 0; x < 2; ++x) {
+			for (int y = 0; y < 2; ++y) {
+				for (int z = 0; z < 2; ++z) {
 					int n1[3] = {0};
 					int n2[3] = {0};
 					n1[0] = x, n1[1] = y, n1[2] = z;
 					n2[0] = x + 1, n2[1] = y + 1, n2[2] = z + 1;
-					result += Adaptive_Rectangular_3D(get_Division(a, b, n1), get_Division(a, b, n2), epsilon / 8, f, level + 1);
+					double * aa, *bb;
+					aa = get_Division(a, b, n1);
+					bb = get_Division(a, b, n2);
+					result += Adaptive_Rectangular_3D(aa, bb, epsilon / 8, f, level + 1);
+					delete aa;
+					delete bb;
 				}
 			}
 		}
-		cout << "return point 2, level=" << level << " a=[" << a[0] << ' ' << a[1] << ' ' << a[2] << ' ' << "], b=[" << b[0] << ' ' << b[1] << ' ' << b[2] << "]" << endl;
+		//cout << "return point 2, level=" << level << " a=[" << a[0] << ' ' << a[1] << ' ' << a[2] << ' ' << "], b=[" << b[0] << ' ' << b[1] << ' ' << b[2] << "]" << endl;
 		return result;
 	}
 	return result;
@@ -75,12 +86,10 @@ int main() {
 	
 	double a[3] = { -1 , -1, -1}, b[3] = { 1, 1, 1 };
 
-	cout << a[0] << ' ' << a[1] << ' ' << a[2] << ' ' << b[0] << ' ' << b[1] << ' ' << b[2] << endl;
-	
 	cout << Adaptive_Rectangular_3D(a, b,0.0001, My_func3, 0) << endl;
 
 	end = clock();
-	cout << end - start << endl;
+	cout << (end - start)/1000.0 << endl;
 	cout << Func_count << endl;
 	system("pause");
 	return 0;
